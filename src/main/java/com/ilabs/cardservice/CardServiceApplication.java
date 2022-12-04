592de2e028144222695d2f5f4c4526454e189606
Pavucs Manokar <smpavucsan@gmail.com>
2022-12-05 00:53:58 +0530

    resource server config added

--- src/main/java/com/ilabs/cardservice/CardServiceApplication.java
+++ src/main/java/com/ilabs/cardservice/CardServiceApplication.java
@@ -4,9 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+//@EnableResourceServer
 public class CardServiceApplication {
-	public static void main(String[] args) {
-		SpringApplication.run(CardServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(CardServiceApplication.class, args);
+    }
 
 }
---
+++ src/main/java/com/ilabs/cardservice/config/WebConfiguration.java
@@ -0,0 +1,30 @@
+package com.ilabs.cardservice.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class WebConfiguration extends WebSecurityConfigurerAdapter {
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers(
+                "/v2/api-docs",
+                "/swagger-ui/index.html",
+                "/configuration/ui",
+                "/swagger-resources/**",
+                "/configuration/security",
+                "/swagger-ui.html",
+                "/swagger-ui/",
+                "/swagger-ui/**",
+                "/webjars/**");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/card/**").access("hasRole('ADMIN')");
+
+    }
+}
